054c4b03fbaddbc4aaa2340bd81cdd96c87f6b9e
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-03 17:38:57 +0300

    disk usage info

--- cmd/info/diskusage.go
+++ cmd/info/diskusage.go
@@ -1,1 +1,19 @@
 package info
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var diskUsageCmd = &cobra.Command{
+	Use:   "diskUsage",
+	Short: "diskusage information",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("diskusage called")
+	},
+}
+
+func init() {
+	InfoCmd.AddCommand(diskUsageCmd)
+}
