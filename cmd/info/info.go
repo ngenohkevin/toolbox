79c900b6037981b6aeb29f7758d34f2e218f0727
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-03 17:39:22 +0300

    info

--- cmd/info/info.go
+++ cmd/info/info.go
@@ -1,1 +1,19 @@
 package info
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var InfoCmd = &cobra.Command{
+	Use:   "info",
+	Short: "All things information",
+	Long:  "",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("info called")
+	},
+}
+
+func init() {
+
+}
