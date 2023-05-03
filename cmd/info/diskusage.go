b06205d6fdc78d733dd9e3ff1519badfd47ab3e5
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-03 17:52:51 +0300

    show disk usage

--- cmd/info/diskusage.go
+++ cmd/info/diskusage.go
@@ -2,15 +2,17 @@ package info
 
 import (
 	"fmt"
+	"github.com/ricochet2200/go-disk-usage/du"
 	"github.com/spf13/cobra"
 )
 
 var diskUsageCmd = &cobra.Command{
 	Use:   "diskUsage",
-	Short: "diskusage information",
+	Short: "prints disk usage of the current directory",
 	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("diskusage called")
+		usage := du.NewDiskUsage(".")
+		fmt.Printf("%v\n", usage)
 	},
 }
 
